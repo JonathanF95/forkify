73a4e70335a8d229192a549547377355df853132
Jonathan Fuentes <john.fuen95@gmail.com>
2024-07-14 20:47:44 -0500

    Add files via upload

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,9 +32,13 @@ const renderSpinner = function(parentEl) {
 }
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1)
+
+    if(!id) return;
+
     //Loading Recipe
     renderSpinner(recipeContainer);
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`)
@@ -147,6 +151,9 @@ const showRecipe = async function () {
     alert(err.message)
   }
 };
+['hashchange', 'load'].forEach(ev => {
+  window.addEventListener(ev, showRecipe)
+})
 
-showRecipe();
+
 
